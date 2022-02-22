c8f5646536dd92bf9e720019b2341a7980dd1d95
Sigit Prasetyo Noprianto <noprisigit@gmail.com>
2022-02-22 13:20:39 +0700

    create delete invoice api

--- Section 1/routes/invoice.js
+++ Section 1/routes/invoice.js
@@ -3,7 +3,9 @@ const validation = require('../middlewares/ValidationInput');
 const invoiceValidationSchema = require('../utils/InvoiceValidationSchema');
 const InvoiceController = require('../controllers/InvoiceController');
 
+
 router.post('/', validation(invoiceValidationSchema), InvoiceController.store);
 router.patch('/:id', validation(invoiceValidationSchema), InvoiceController.update);
+router.delete('/:id', InvoiceController.destroy);
 
 module.exports = router;
