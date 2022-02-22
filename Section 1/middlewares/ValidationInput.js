d1a5fe58ee781203855e5b1523503cf56d9b7220
Sigit Prasetyo Noprianto <noprisigit@gmail.com>
2022-02-22 12:35:51 +0700

    create middleware validation input

---
+++ Section 1/middlewares/ValidationInput.js
@@ -0,0 +1,13 @@
+function validation(schema) {
+  return async (req, res, next) => {
+    try {
+      const validatedBody = await schema.validate(req.body);
+      req.body = validatedBody;
+      next();
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  }
+}
+
+module.exports = validation;
