9ee407e14f3928c453e9ebe0e1d79cc25cc38048
Sigit Prasetyo Noprianto <noprisigit@gmail.com>
2022-02-22 12:11:16 +0700

    create product seeder

---
+++ Section 1/seeders/ProductSeeder.js
@@ -0,0 +1,34 @@
+const products = [
+  {
+    itemName: "Bluetooth Speaker",
+    quantity: 3,
+    totalCostOfGoodsSold: 630000,
+    totalPriceSold: 756000,
+  },
+  {
+    itemName: "Headphone",
+    quantity: 8,
+    totalCostOfGoodsSold: 400000,
+    totalPriceSold: 480000,
+  },
+  {
+    itemName: "Laptop charger",
+    quantity: 4,
+    totalCostOfGoodsSold: 800000,
+    totalPriceSold: 960000,
+  },
+  {
+    itemName: "LCD Monitor",
+    quantity: 1,
+    totalCostOfGoodsSold: 500000,
+    totalPriceSold: 600000,
+  },
+  {
+    itemName: "Laptop charger",
+    quantity: 4,
+    totalCostOfGoodsSold: 800000,
+    totalPriceSold: 960000,
+  },
+];
+
+module.exports = products;
